381ba3763ada060b89e513b0a7c60225d890ff31
Parker Bacall <pbacall2@gmail.com>
2019-11-18 22:32:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -157,7 +157,7 @@ def player_numbers(input) # takes in argument of team returns all players jersey
   array = [] #create new array which will be full of player numbers
   game_hash.each do |team, team_info| #split the game hash into keys team and team info and iterate through them
    if input == team_info[:team_name] # if the input matches the team_name: value for either team
-      team_info[:players].each do |player| #for index in the array players
+      team_info[:players].map do |player| #for index in the array players
         player[:number] #add each player number to the array
         end
       end
